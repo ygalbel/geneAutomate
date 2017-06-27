0772b6632db05dc933f873faa666c75765285c26
PAYONEER\ygalbe <ygalbe@payoneer.com>
2017-06-27 23:38:14 +0300

    Add validations

---
+++ GeneAutomate.BusinessLogic/BooleanNetworkValidator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeneAutomate.Models;
+
+namespace GeneAutomate.BusinessLogic
+{
+    public class BooleanNetworkValidator
+    {
+        public bool IsValidAutomata(GeneNode automata, Condition currentStatus, List<GeneLink> booleanNetwok)
+        {
+            if (automata == null || automata.Transitions == null || !automata.Transitions.Any())
+            {
+                return true;
+            }
+
+            var isValid = automata.Transitions.All(t => 
+            IsValidTransition(currentStatus, t.Condition, booleanNetwok) && IsValidAutomata(t.Node, t.Condition, booleanNetwok));
+
+            return isValid;
+        }
+
+        private bool IsValidTransition(Condition currentStatus, Condition newStatus, List<GeneLink> booleanNetwok)
+        {
+            if (currentStatus == null)
+            {
+                return true;
+            }
+
+            var importantRules = booleanNetwok.Where(a => !a.IsOptional).ToList();
+
+            var isFine = importantRules.All(r =>
+            {
+                var c = currentStatus.FirstOrDefault(a => a.Key == r.From);
+                var n = newStatus.FirstOrDefault(a => a.Key == r.To);
+
+                if (!c.Value.HasValue || !n.Value.HasValue)
+                {
+                    return true;
+
+                }
+
+                if (r.IsPositive)
+                {
+                    return c.Value.Value == n.Value.Value;
+                }
+                else
+                {
+                    return c.Value.Value == !n.Value.Value;
+                }
+            });
+
+
+            return isFine;
+        }
+
+       
+    }
+}
---
+++ GeneAutomate.Models/GeneAutomata.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneAutomate.Models
+{
+    // TODO: change structure to Node and Transition, to allow loops
+    public class GeneNode
+    {
+        public string NodeName { get; set; }
+
+        public List<GeneTransition> Transitions { get; set; }
+    }
+
+    public class GeneTransition
+    {
+        public GeneNode Node { get; set; }
+
+        public Condition Condition { get; set; }
+    }
+}
